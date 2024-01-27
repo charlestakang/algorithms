0e9fef943b32d46c2dbbd5d3e174368ddac19146
charles.takang x1c7 <charles.takang@gmail.com>
2024-01-27 07:36:57 +0100

    Refactored main.cpp to calculate and display the sum of digits

--- main.cpp
+++ main.cpp
@@ -6,10 +6,18 @@ int main()
     int num;
     std::cout << "Enter a number\n";
     std::cin >> num;
-    int lnum = num%10;
-    int fnum = num/10;
-    std::cout << "You entered " << num << "\n";
-    std::cout << "First digit is " << fnum << "\n";
-    std::cout << "Last digit is " << lnum << "\n";
+    int sum = 0;
+    while (num != 0)
+    {
+        /* code */
+        int lnum = num%10;
+        int fnum = num/10;
+        std::cout << "You entered " << num << "\n";
+        std::cout << "First digit is " << fnum << "\n";
+        std::cout << "Last digit is " << lnum << "\n";
+        sum += lnum;
+        num = fnum;
+    }
+    std::cout << "Sum of digits is " << sum << "\n";
     return 0;
 }
